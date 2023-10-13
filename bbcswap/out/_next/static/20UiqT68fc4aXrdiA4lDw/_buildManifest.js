5cf3b0d4846d1d27e46a8bd64132e05fcade4dc2
Beo857 <94695178+Beo857@users.noreply.github.com>
2023-10-12 21:29:45 -0400

    Add files via upload

---
+++ bbcswap/out/_next/static/20UiqT68fc4aXrdiA4lDw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/720-e2ae0276a8b42eca.js","static/css/0e03a697f20de1b5.css","static/chunks/pages/index-193945baae623661.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ bbcswap/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Wallet from '../components/wallet'
+import ProviderExample from '../components/provider'
+
+export default function Home() {
+    return (
+        <>
+            <ProviderExample />
+            <Wallet />
+        </>
+    )
+}
